7bfe522bb013bfc50004605f3011b24776e888f0
Yodahe Maaza <yodahem24@lakesideschool.org>
2023-07-13 14:03:32 -0700

    git hub practice

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .foregroundColor(Color.pink)
                 .padding(.horizontal)
-                
+                Text("hey")
                 
                 
                 
